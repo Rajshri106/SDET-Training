b9aeca35bea5a6eb5b5195b40120ae73e294d657
Rajshri Mate <rajshrimate111@gmail.com>
2023-03-10 14:00:01 +0530

    first assignment added

--- src/FirstAssignment.java
+++ src/FirstAssignment.java
@@ -1,65 +1,65 @@
 
 
 public class FirstAssignment {
- 
 
-public static void areOccurrencesEqual(String s) { 
 
+	public static void areOccurrencesEqual(String s) { 
 	
-	    int maxSize = 100;
-		int count1[] = new int[maxSize];
-		int count2[] = new int[0];
-		int len = s.length();
-        // Initialize count array index
-        for ( int i = 0; i < s.length(); i++)
-        {    
-        	count1[s.charAt(i)]++;
-
-        }
-        
-        //traverse string
-        char letter[] = new char[s.length()];
-        for(int i=0;i<=len;i++)
-		{
+		
+		    int maxSize = 100;
+			int count1[] = new int[maxSize];
+			int count2[] = new int[0];
+			int len = s.length();
+	        // Initialize count array index
+	        for ( int i = 0; i < s.length(); i++)
+	        {    
+	        	count1[s.charAt(i)]++;
+	
+	        }
 	        
-        	letter[i] = s.charAt(i);
-			
-			int flag = 0;
-			int j=0;
-			
-			while(j<=i)
+	        //traverse string
+	        char letter[] = new char[s.length()];
+	        for(int i=0;i<=len;i++)
 			{
+		        
+	        	letter[i] = s.charAt(i);
 				
-				if(s.charAt(i)==letter[j])
+				int flag = 0;
+				int j=0;
+				
+				while(j<=i)
 				{
-					flag++;
+					
+					if(s.charAt(i)==letter[j])
+					{
+						flag++;
+						
+					}
+					j++;
 					
 				}
-				j++;
 				
-			}
-			
-			if(flag==1)
-			{
-			
-			System.out.println("count total number of"+" "+letter[i]+" =  "+count1[s.charAt(i)]);
-			
-			}
-        } 
-        if(count1[0] == count1[1] && count1[1] == count1[2] && count1[2] == count1[0])
-        {
-        	System.out.println("The total occurances are equal is ="+count1[0]);
-        }
-}
-	
-	public static void main(String[] args)
-    {
-        
-        String s = "bbcabc";
- 
-        areOccurrencesEqual(s);
-    }
-	
-	
-	
+				if(flag==1)
+				{
+				
+				System.out.println("count total number of"+" "+letter[i]+" =  "+count1[s.charAt(i)]);
+				
+				}
+	        } 
+	        
 	}
+		
+		public static void main(String[] args)
+	    {
+	        
+	        String s = "bbcabc";
+	 
+	        areOccurrencesEqual(s);
+	    }
+		
+		
+		
+		}
+
+
+	
