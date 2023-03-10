ce1bf72e2a77c39c46c9f6788b2e293a9e03ed39
Rajshri Mate <rajshrimate111@gmail.com>
2023-03-10 14:14:23 +0530

    first assignment added

---
+++ src/Assignment1.java
@@ -0,0 +1,65 @@
+
+
+public class Assignment1 {
+ 
+
+public static void areOccurrencesEqual(String s) { 
+
+	
+	    int maxSize = 100;
+		int count1[] = new int[maxSize];
+		int count2[] = new int[0];
+		int len = s.length();
+        // Initialize count array index
+        for ( int i = 0; i < s.length(); i++)
+        {    
+        	count1[s.charAt(i)]++;
+
+        }
+        
+        //traverse string
+        char letter[] = new char[s.length()];
+        for(int i=0;i<=len;i++)
+		{
+	        
+        	letter[i] = s.charAt(i);
+			
+			int flag = 0;
+			int j=0;
+			
+			while(j<=i)
+			{
+				
+				if(s.charAt(i)==letter[j])
+				{
+					flag++;
+					
+				}
+				j++;
+				
+			}
+			
+			if(flag==1)
+			{
+			
+			System.out.println("count total number of"+" "+letter[i]+" =  "+count1[s.charAt(i)]);
+			
+			}
+        } 
+        
+}
+	
+	public static void main(String[] args)
+    {
+        
+        String s = "bbcabc";
+ 
+        areOccurrencesEqual(s);
+    }
+	
+	
+	
+	}
+
+
+
