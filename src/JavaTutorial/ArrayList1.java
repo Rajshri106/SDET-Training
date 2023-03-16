25bbb997635b9282d191b0a293f84dbacee76a31
Rajshri Mate <rajshrimate111@gmail.com>
2023-03-16 13:29:11 +0530

    added code for collections in java

---
+++ src/JavaTutorial/ArrayList1.java
@@ -0,0 +1,34 @@
+package JavaTutorial;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList1 {
+
+	public static void main(String[] args)
+	{
+		List<Integer> arrayList = new ArrayList<Integer>(5);
+		
+		for(int i=0;i<=5;i++)
+		{
+			arrayList.add(i);
+		}
+		//printing elements
+		System.out.println(arrayList);
+		
+		//remove element from index
+		System.out.println(arrayList.remove(3));
+		
+		//printing element one by one
+		for(int i=0; i<arrayList.size();i++)
+		{
+			System.out.println(arrayList.get(i));
+		}
+		
+		//or use enhanced for loop
+//		for(int i:arrayList)
+//		{
+//			System.out.println(i+" ");
+//		}
+	}
+}
---
+++ src/JavaTutorial/TreeMapDemo.java
@@ -0,0 +1,22 @@
+package JavaTutorial;
+
+import java.util.TreeMap;
+
+public class TreeMapDemo {
+
+	
+	public static void main(String[] args)
+	{
+		TreeMap<Integer, String> treemap = new TreeMap<>();
+		
+		treemap.put(3,"A");
+		treemap.put(2, "B");
+		treemap.put(1, "C");
+		
+		
+		System.out.println(treemap);//print element 
+		System.out.println(treemap.size());
+		
+		
+	}
+}
