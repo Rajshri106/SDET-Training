89353b4e94151f3eec131cb7323dfc6164d413cf
Rajshri Mate <rajshrimate111@gmail.com>
2023-03-17 21:29:46 +0530

    add Queue file

---
+++ src/JavaTutorial/QueueDemo.java
@@ -0,0 +1,31 @@
+package JavaTutorial;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueDemo {
+
+	
+	public static void main(String[] args)
+	{
+		Queue<String> queue = new LinkedList<>();
+		
+		queue.add("America");
+		queue.add("Germany");
+		queue.add("India");
+		
+		System.out.println("Queue: "+queue);
+		
+		System.out.println("popped element:" +queue.remove());
+		
+		// To view the head of queue
+        String head = queue.peek();
+        System.out.println("head of queue-"
+                           + head);
+  
+        int size = queue.size();
+        System.out.println("Size of queue-"
+                           + size);
+		
+	}
+}
