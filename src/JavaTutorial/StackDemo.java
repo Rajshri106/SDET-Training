8be7561eeab441c8917462556d5438276def15d5
Rajshri Mate <rajshrimate111@gmail.com>
2023-03-17 21:28:15 +0530

    add stack file

---
+++ src/JavaTutorial/StackDemo.java
@@ -0,0 +1,20 @@
+package JavaTutorial;
+
+import java.util.Stack;
+
+public class StackDemo {
+
+	public static void main(String[] args)
+	{
+		Stack<String> stack = new Stack();
+		
+		stack.push("America");
+		stack.push("Germany");
+		stack.push("India");
+		
+		System.out.println("Stack: "+stack);
+		
+		System.out.println("popped element:" +stack.pop());
+		
+	}
+}
