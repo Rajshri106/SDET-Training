ba55d00ea21b003a22dcfaefa3090266b8ccd1bf
Rajshri Mate <rajshrimate111@gmail.com>
2023-03-22 13:43:12 +0530

    Medium Difficulty Assignment1 updated

--- src/MediumDificultyAssignment/Assignment1.java
+++ src/MediumDificultyAssignment/Assignment1.java
@@ -1,3 +1,4 @@
+package MediumDificultyAssignment;
 
 /*Q.1 Write a java program to find tax for employee. The program should
 calculate the income tax to be paid by the employee as per the criteria given below: 
@@ -7,60 +8,36 @@ Above Rs. 7,00,000 (0-5,00,000=nil,5,00,000 – 7,00,000=10%) 
 Rs. 7,00,000-10,00,000 15% on additional amount 
 Rs. 10,00,000-15,00,000 20% on additional amount Above 15,00,000 30% on additional amount
 Result: - income tax is ……………………………. */
-
-
-
-package MediumDificultyAssignment;
-
-import java.util.Scanner;
-
 public class Assignment1 {
-    public void findIncomeTax() {
-        //get input value from user using scanner
-        Scanner in = new Scanner(System.in);
-
-        System.out.print("Enter Employee Code: ");
-        int empCode = in.nextInt();
-        System.out.print("Enter Total Annual Income: ");
-        double totalIncome = in.nextDouble();
-        System.out.print("Enter Deduction Annual Savings: ");
-        double deduction = in.nextDouble();
+    public void findIncomeTax(double totalIncome, double deduction) {
 
         double taxableIncome = totalIncome - deduction;
         int taxRate =0;
         switch (String.valueOf(taxableIncome)) {
             case "(taxableIncome <= 500000)":
                 taxRate = 0;
-
                 break;
             case "(taxableIncome >= 500000 && taxableIncome <= 700000)":
-//                tax = taxableIncome * taxRate / 100;
-//                System.out.println("Income Tax of the employee is: " + tax);
                 taxRate=15;
                 break;
             case "(taxableIncome >= 700000 && taxableIncome <= 1000000)":
-//                tax = taxableIncome * taxRate / 100;
-//                System.out.println("Income Tax of the employee is: " + tax);
                 taxRate=20;
                 break;
             case "(taxableIncome >= 1000000 && taxableIncome <= 1500000)":
-//                tax = taxableIncome * taxRate / 100;
-//                System.out.println("Income Tax of the employee is: " + tax);
                 taxRate=20;
                 break;
             default:
                 taxRate = 30;
         }
 
-        double tax = taxableIncome * taxRate / 100;
-
+        double tax = (taxableIncome * taxRate) / 100;
         System.out.println("Income Tax of the employee is: " + tax);
-
-
     }
 
     public static void main(String[] args) {
         Assignment1 a = new Assignment1();
-        a.findIncomeTax();
+        double annualIncome = 700000;
+        double annualDeduction = 50000;
+        a.findIncomeTax(annualIncome, annualDeduction);
     }
 }
