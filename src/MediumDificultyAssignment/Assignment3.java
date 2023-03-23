ac1d5bbd92181c8f14447bda66005d0ec87c261a
Rajshri Mate <rajshrimate111@gmail.com>
2023-03-23 14:08:02 +0530

    Medium Dificulty Assignment 3 updated

--- src/MediumDificultyAssignment/Assignment3.java
+++ src/MediumDificultyAssignment/Assignment3.java
@@ -7,52 +7,34 @@ swapEnds([1, 6, 7, 9, 8]) → [1, 6, 7, 9, 8]   */
 
 package MediumDificultyAssignment;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class Assignment3 {
 
-    public void calculatePrimeNum() {
-        //create linked list
-        LinkedList<Integer> array = new LinkedList<>();
-
-        // Add elements to LinkedList
-        array.add(1);
-        array.add(2);
-        array.add(3);
-        array.add(7);
-        //print array
-        System.out.println("LinkedList : " + array);
-        //get 1st index value
-        int first = array.get(0);
-        //calculate size of array
-        int len = array.size();
-        //get last index value
-        int last = array.get(len - 1);
-        //remove first value from array
-        int removeFirst = array.removeFirst();
-        //remove last value from array
-        int removeLast = array.removeLast();
-
-        //add element to 1st index
-        array.addFirst(last);
-        //add element to last index
-        array.addLast(first);
-
-        System.out.println("Swap first and last element : " + array);
-        //get last value from array
-        int lastNum = array.get(len - 1);
-
-        //check the last value is prime or not
+    public void calculatePrimeNum(int[] intArray) {
+        int temp = 0;
+        int lastNum = intArray[intArray.length - 1];
         if (lastNum % 2 == 0) {
             System.out.println("The last number of array is not prime number: " + lastNum);
-        } else
+        }
+        else {
+            temp = intArray[0];
+            intArray[0] = intArray[intArray.length - 1];
+            intArray[intArray.length - 1] = temp;
             System.out.println("The last number of array is  prime number: " + lastNum);
-
+            System.out.println("The swap array is: " + Arrays.toString(intArray));
+        }
     }
 
     public static void main(String[] args) {
         Assignment3 a = new Assignment3();
-        a.calculatePrimeNum();
+        int[] intArray = new int[]{1, 2, 3, 7};
+        int[] intArray1 = new int[]{1, 2, 3};
+        int[] intArray2 = new int[]{1, 6, 7, 9, 8};
+        a.calculatePrimeNum(intArray);
+        a.calculatePrimeNum(intArray1);
+        a.calculatePrimeNum(intArray2);
     }
 
 
