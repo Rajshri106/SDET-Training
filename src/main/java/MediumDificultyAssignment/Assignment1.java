1e9171d397f1a2baf65a0f7887d84623e667749b
Rajshri Mate <rajshrimate111@gmail.com>
2023-04-04 12:27:43 +0530

    Medium Complexity assignment1 updated

--- src/main/java/MediumDificultyAssignment/Assignment1.java
+++ src/main/java/MediumDificultyAssignment/Assignment1.java
@@ -9,35 +9,40 @@ Rs. 7,00,000-10,00,000 15% on additional amount 
 Rs. 10,00,000-15,00,000 20% on additional amount Above 15,00,000 30% on additional amount
 Result: - income tax is ……………………………. */
 public class Assignment1 {
-    public void findIncomeTax(double totalIncome, double deduction) {
+    private double totalTax;
+	private int IncomeCases;
 
-        double taxableIncome = totalIncome - deduction;
-        int taxRate =0;
-        switch (String.valueOf(taxableIncome)) {
-            case "(taxableIncome <= 500000)":
-                taxRate = 0;
-                break;
-            case "(taxableIncome >= 500000 && taxableIncome <= 700000)":
-                taxRate=15;
-                break;
-            case "(taxableIncome >= 700000 && taxableIncome <= 1000000)":
-                taxRate=20;
-                break;
-            case "(taxableIncome >= 1000000 && taxableIncome <= 1500000)":
-                taxRate=20;
-                break;
-            default:
-                taxRate = 30;
-        }
+        	public void findIncomeTax(int taxableIncome) {
+	IncomeCases = (taxableIncome <= 700000) ? 1 : (taxableIncome > 500000 && taxableIncome <= 1000000) ? 2 : (taxableIncome <= 1500000) ? 3 : 4;
 
-        double tax = (taxableIncome * taxRate) / 100;
-        System.out.println("Income Tax of the employee is: " + tax);
-    }
+	switch (IncomeCases) {
+	case 1:
+	totalTax = 0;
+	break;
+	case 2:
+	totalTax = (0.15 * (taxableIncome - 700000)) + (0.1 * 200000);
+	break;
+	case 3:
+	totalTax = (0.2 * (taxableIncome - 1000000)) + (0.15 * 300000) + (0.1 * 200000);
+	break;
+	case 4:
+	totalTax = (0.3 * (taxableIncome - 1500000)) + (0.2 * 500000) + (0.15 * 300000) + (0.1 * 200000);
+	break;
+	default:
+	System.out.println("Enter income");
+	}
+	System.out.println("Income tax amount is :" + totalTax);
+	}
 
-    public static void main(String[] args) {
-        Assignment1 a = new Assignment1();
-        double annualIncome = 400000;
-        double annualDeduction = 50000;
-        a.findIncomeTax(annualIncome, annualDeduction);
-    }
+        	public static void main(String[] args) {
+	Assignment1 incomeTaxCalculation = new Assignment1();
+	System.out.println("\nTotal Taxable Income : 700000");
+	incomeTaxCalculation.findIncomeTax(700000);
+	System.out.println("\nTotal Taxable Income : 800000");
+	incomeTaxCalculation.findIncomeTax(800000);
+	System.out.println("\nTotal Taxable Income : 1400000");
+	incomeTaxCalculation.findIncomeTax(1400000);
+	System.out.println("\nTotal Taxable Income : 1600000");
+	incomeTaxCalculation.findIncomeTax(1600000);
+	}
 }
