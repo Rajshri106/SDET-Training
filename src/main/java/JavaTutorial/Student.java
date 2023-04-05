d293f620a24108dab98123db12b6193fe57f19f2
Rajshri Mate <rajshrimate111@gmail.com>
2023-04-05 16:05:27 +0530

    Student code updated

--- src/main/java/JavaTutorial/Student.java
+++ src/main/java/JavaTutorial/Student.java
@@ -1,18 +1,14 @@
 package JavaTutorial;
-
 public class Student {
-
-	String name;
-	int age;
-	String address;
-	
+	private String name;
+	private int age;
+	private String address;
 	public Student(String name, int age, String address)
 	{
 		this.name = name;
 		this.age = age;
 		this.address = address;
 	}
-	
 	public void setName(String name)
 	{
 		this.name = name;
@@ -40,7 +36,6 @@ public class Student {
 	public static void main(String[] args)
 	{
 		Student s = new Student("abc",12,"In");
-		
 		System.out.println(s.getName());
 		System.out.println(s.getAge());
 		System.out.println(s.getAddress());
