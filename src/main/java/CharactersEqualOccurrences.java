a385a11e9f270665f5827c2e2b6211cb6b0af7cc
Rajshri Mate <rajshrimate111@gmail.com>
2023-04-05 15:47:04 +0530

    character equal occurences code updated

--- src/main/java/CharactersEqualOccurrences.java
+++ src/main/java/CharactersEqualOccurrences.java
@@ -1,18 +1,12 @@
-
-
 public class CharactersEqualOccurrences {
-
-
     public boolean areOccurrencesEqual(String inputString) {
         int size = inputString.length();
         int[] counts = new int[size];
         int max = 0;
-
         for (char inputChar : inputString.toCharArray()) {
             counts[inputChar - 'a']++;
             max = Math.max(max, counts[inputChar - 'a']);
         }
-
         for (int count : counts) {
             if (count != 0 && count != max) {
                 return false;
@@ -28,8 +22,4 @@ public class CharactersEqualOccurrences {
         System.out.println("String abacbc is : " + charactersEqualOccurrences.areOccurrencesEqual(firstInput));
         System.out.println("String abacbc is : " + charactersEqualOccurrences.areOccurrencesEqual(SecondInput));
     }
-
 }
-
-
-
