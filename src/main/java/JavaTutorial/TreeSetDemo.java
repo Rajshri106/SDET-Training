4108c0149ec67fa2907ded4997a2f063b1ff9095
Rajshri Mate <rajshrimate111@gmail.com>
2023-04-06 12:26:07 +0530

    Tree Set Demo code updated

--- src/main/java/JavaTutorial/TreeSetDemo.java
+++ src/main/java/JavaTutorial/TreeSetDemo.java
@@ -1,10 +1,8 @@
 package JavaTutorial;
 
-import java.util.HashSet;
 import java.util.TreeSet;
 
 public class TreeSetDemo {
-
     public static void main(String[] args) {
         TreeSet<String> treeSet = new TreeSet<String>();
         treeSet.add("B");
