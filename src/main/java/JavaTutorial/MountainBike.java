28541ba0bab1b7cf26405b1c84b34aa3e175fa4e
Rajshri Mate <rajshrimate111@gmail.com>
2023-04-06 12:11:39 +0530

    Mountain Bike code updated

--- src/main/java/JavaTutorial/MountainBike.java
+++ src/main/java/JavaTutorial/MountainBike.java
@@ -2,12 +2,10 @@ package JavaTutorial;
 
 public class MountainBike extends Inheritance {
     public int setHeight;
-
     public MountainBike(int startHeight, int startSpeed, int startGear) {
         super(startSpeed, startGear);
         this.setHeight = startHeight;
     }
-
     public void setHeight(int newValue) {
         setHeight = newValue;
     }
