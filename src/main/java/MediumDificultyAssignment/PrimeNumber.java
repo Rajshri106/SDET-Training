a2be80258491f06de8f31e061da2004732b27d79
Rajshri Mate <rajshrimate111@gmail.com>
2023-04-10 13:18:12 +0530

    prime number code updated

--- src/main/java/MediumDificultyAssignment/PrimeNumber.java
+++ src/main/java/MediumDificultyAssignment/PrimeNumber.java
@@ -1,19 +1,30 @@
 package MediumDificultyAssignment;
 
 import java.util.Arrays;
+
 public class PrimeNumber {
     public void calculatePrimeNum(int[] intArray) {
-        int temp = 0;
         int lastNum = intArray[intArray.length - 1];
-        if (lastNum % 2 == 0) {
+        if (lastNum <= 1)
+            System.out.println("The last number of array is not prime number: " + lastNum);
+        else if (lastNum == 2) {
+            swapArray(intArray);
+            System.out.println("The last number of array is  prime number: " + lastNum);
+            System.out.println("The swap array is: " + Arrays.toString(intArray));
+        } else if (lastNum % 2 == 0) {
             System.out.println("The last number of array is not prime number: " + lastNum);
         } else {
-            temp = intArray[0];
-            intArray[0] = intArray[intArray.length - 1];
-            intArray[intArray.length - 1] = temp;
+            swapArray(intArray);
             System.out.println("The last number of array is  prime number: " + lastNum);
             System.out.println("The swap array is: " + Arrays.toString(intArray));
         }
+    }
+    public void swapArray(int intArray[])
+    {
+        int temp=0;
+        temp = intArray[0];
+        intArray[0] = intArray[intArray.length - 1];
+        intArray[intArray.length - 1] = temp;
     }
 
     public static void main(String[] args) {
