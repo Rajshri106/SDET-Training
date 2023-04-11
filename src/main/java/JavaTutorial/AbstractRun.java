e46ac191b7b64b5b257741cb24b7e6e0002ec29e
Rajshri Mate <rajshrimate111@gmail.com>
2023-04-11 16:02:02 +0530

    code updated

--- src/main/java/JavaTutorial/AbstractRun.java
+++ src/main/java/JavaTutorial/AbstractRun.java
@@ -2,11 +2,10 @@ package JavaTutorial;
 
 public class AbstractRun {
 
-	public static void main(String[] args)
-	{
-		AbstractDemo abstractCircle = new AbstractCircle();
-		abstractCircle.moveTo(4,7);
-		abstractCircle.draw();
-		abstractCircle.resize();
-	}
+    public static void main(String[] args) {
+        AbstractDemo abstractCircle = new AbstractCircle();
+        abstractCircle.moveTo(4, 7);
+        abstractCircle.draw();
+        abstractCircle.resize();
+    }
 }
