038d2a27636e0d333e9a8f5f49e43249231eb2c8
Rajshri Mate <rajshrimate111@gmail.com>
2023-04-11 17:19:20 +0530

    code updated

--- src/main/java/MediumDificultyAssignment/PrimeNumber.java
+++ src/main/java/MediumDificultyAssignment/PrimeNumber.java
@@ -4,21 +4,24 @@ import java.util.Arrays;
 
 public class PrimeNumber {
     public void calculatePrimeNum(int[] inputArray) {
-        boolean flag = false;
         int lastNum = inputArray[inputArray.length - 1];
-        if (lastNum <= 1)
-            System.out.println("The last number of array is not prime number: " + lastNum);
-        for (int index = 2; index <= lastNum / 2; index++) {
-            if (lastNum % index == 0) {
+        int resultNum = lastNum <= 1 ? 1 : lastNum == 2 ? 2 : 3;
+        switch (resultNum) {
+            case 1:
                 System.out.println("The last number of array is not prime number: " + lastNum);
-                flag = true;
                 break;
-            }
-        }
-        if (!flag) {
-            swapArray(inputArray);
-            System.out.println("The last number of array is  prime number: " + lastNum);
-            System.out.println("The swap array is: " + Arrays.toString(inputArray));
+            case 2:
+                System.out.println("The last number of array is prime number: " + lastNum);
+                break;
+            case 3:
+                if ((lastNum / 2) % 2 == 0) {
+                    System.out.println("The last number of array is not prime number: " + lastNum);
+                } else {
+                    swapArray(inputArray);
+                    System.out.println("The last number of array is  prime number: " + lastNum);
+                    System.out.println("The swap array is: " + Arrays.toString(inputArray));
+                }
+                break;
         }
     }
 
@@ -31,7 +34,7 @@ public class PrimeNumber {
 
     public static void main(String[] args) {
         PrimeNumber primeNumber = new PrimeNumber();
-        int[] inputArray = new int[]{1, 2, 3, 9};
+        int[] inputArray = new int[]{1, 2, 3, 7};
         int[] inputArray1 = new int[]{1, 2, 3};
         int[] inputArray2 = new int[]{1, 6, 7, 9, 8};
         primeNumber.calculatePrimeNum(inputArray);
