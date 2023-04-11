c80bf6918dd957992f6828e1c5c0e3c604cd175d
Rajshri Mate <rajshrimate111@gmail.com>
2023-04-11 15:52:24 +0530

    student code updated

--- src/main/java/JavaTutorial/Student.java
+++ src/main/java/JavaTutorial/Student.java
@@ -1,43 +1,44 @@
 package JavaTutorial;
+
 public class Student {
-	private String name;
-	private int age;
-	private String address;
-	public Student(String name, int age, String address)
-	{
-		this.name = name;
-		this.age = age;
-		this.address = address;
-	}
-	public void setName(String name)
-	{
-		this.name = name;
-	}
-	public void setAge(int age)
-	{
-		this.age = age;
-	}
-	public void setAddress(String address)
-	{
-		this.address = address;
-	}
-	public String getName()
-	{
-		return name;
-	}
-	public int getAge()
-	{
-		return age;
-	}
-	public String getAddress()
-	{
-		return address;
-	}
-	public static void main(String[] args)
-	{
-		Student stud = new Student("abc",12,"In");
-		System.out.println(stud.getName());
-		System.out.println(stud.getAge());
-		System.out.println(stud.getAddress());
-	}
+    private String name;
+    private int age;
+    private String address;
+
+    public Student(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public static void main(String[] args) {
+        Student stud = new Student("abc", 12, "In");
+        System.out.println(stud.getName());
+        System.out.println(stud.getAge());
+        System.out.println(stud.getAddress());
+    }
 }
